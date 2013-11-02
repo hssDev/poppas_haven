30253611d8dcaf11d1db7249c8e87dbd88e54f3b
Michael Hensley <hensley2@pdx.edu>
2013-11-01 18:45:21 -0700

    Added and documented new error message class that displays an error message in a dialog box to the user and then exits the calling activity

---
+++ src/com/hssdevelopment/poppas_haven/Error_Message.java
@@ -0,0 +1,58 @@
+package com.hssdevelopment.poppas_haven;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+
+/*Purpose: Error Message can be invoked in an activity to display 
+ * an error message in a dialog box to the user by passing to this class the
+ * message string and the current activity context, and calling the showErrorMessage function.
+ * Immediately after use, the calling activity should call finish() to close the activity.
+ */
+public class Error_Message 
+{
+	private Activity UI_Context;
+	private AlertDialog.Builder messageBuilder;
+	private String errorMessage;
+	
+	//Error message to show at the top of the dialog box
+	private static final String ERROR_MESSAGE_TITLE = "An Error Has Occured";
+	
+	Error_Message(Activity UI_Context, String errorMessage)
+	{
+		this.UI_Context = UI_Context;
+		this.errorMessage = errorMessage;
+	}
+	
+	/*showErrorMessage()
+	 * Builds an Alert Dialog Box that shows an error message to the user with 
+	 * an ok button. Immediately after calling this function, the calling activity
+	 * should call finish()
+	 */
+	
+	public void showErrorMessage()
+	{
+		//Create new Dialog builder
+		messageBuilder = new AlertDialog.Builder(UI_Context);
+		
+		//Set positive button to okay and set okay button click to close
+		//current activity
+		messageBuilder.setPositiveButton("Okay", new OnClickListener()
+		{
+			@Override
+			public void onClick(DialogInterface dialog, int which) 
+			{
+				UI_Context.finish();
+			}
+		});
+		
+		//Set Error Message Title
+		messageBuilder.setTitle(ERROR_MESSAGE_TITLE);
+		
+		//Set Error Message
+		messageBuilder.setMessage(errorMessage);
+		
+		messageBuilder.show();
+	}
+}
